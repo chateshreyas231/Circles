d1c2b5c97f73e0d9643760ac26108c0afd1e8fb8
Shreyas Chate <chateshreyas231@gmail.com>
2024-05-18 09:01:49 -0500

    frontend and backend connection working

---
+++ CirclesBackend/src/server.ts
@@ -0,0 +1,17 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import app from "./app";
+import notifier from "node-notifier";
+import config from "./config/env";
+import { Socket } from "socket.io";
+import io from "./modules/socket/socket";
+import IO from "./modules/socket/socket";
+
+process.on("uncaughtException", () => {});
+process.on("unhandledRejection", () => {});
+console.log(process.env.NODE_ENV);
+IO;
+
+app.listen(config.port, () => {
+  console.log(" Server Started @:", config.port);
+});
